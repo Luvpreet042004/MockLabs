aa6e23c67b9ae33c480875279f713109a51f8e56
Luvpreet Singh <luvpreet_s@ma.iitr.ac.in>
2025-06-24 15:22:57 +0530

    send email removed

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,14 +3,12 @@ import UserRouter from './userRoutes'
 import SolRouter from './Soution'
 import userAuth from '../middlewares/userAuth'
 import testRouter from './testRoutes'
-import pRoutes from './sendEmail'
 
 const router = Router();
 
 router.use('/user',UserRouter)
 router.use('/solution',userAuth,SolRouter)
 router.use('/test',userAuth,testRouter)
-router.use('/portfolio',pRoutes)
 
 
 
